75c2585789bf3f9d65b531ca699303d4b1451754
Lucas8x <lucasemanuel8x@gmail.com>
2022-01-02 00:57:55 -0300

    Add maps imgs download

--- extractor.py
+++ extractor.py
@@ -1,12 +1,16 @@
 import json
 import os
 from typing import Optional
-from lxml import html
+
 import requests
+from lxml import html
+
+from utils import download_img
 
 
 USE_FILTER = False
 NO_ICONS = False
+NO_MAP_IMGS = False
 IGNORE_EMPTY_MAPS = False
 HEADERS = {
   'Accept-Language': 'en-US'
@@ -14,10 +18,40 @@ HEADERS = {
 STATS_FILTER = ['level', 'health', 'baseExperience', 'jobExperience']
 
 
+def get_mvp_icon(mvp_id: str) -> None:
+  download_img(
+    f'./mvps_icons/{mvp_id}.png',
+    f'https://static.divine-pride.net/images/mobs/png/{mvp_id}.png',
+    'Icon already exists, skipping...',
+    f'[{mvp_id}] Downloading mvp icon {mvp_id}.png',
+    f'[{mvp_id}] Completed download mvp icon {mvp_id}.png',
+    f'[{mvp_id}] Failed to download mvp icon {mvp_id}.png',
+  )
+
+
+def get_map_img(map_name: str, mvp_id: str) -> None:
+  download_img(
+    f'./maps/{map_name}.png',
+    f'https://www.divine-pride.net/img/map/original/{map_name}',
+    f'[{mvp_id}] Map img already exists, skipping...',
+    f'[{mvp_id}] Downloading map {map_name}.png',
+    f'[{mvp_id}] Completed download map {map_name}.png',
+    f'[{mvp_id}] Failed to download map {map_name}.png'
+  )
+  download_img(
+    f'./maps/{map_name}_raw.png',
+    f'https://www.divine-pride.net/img/map/raw/{map_name}',
+    f'[{mvp_id}] Raw Map img already exists, skipping...',
+    f'[{mvp_id}] Downloading raw map {map_name}.png',
+    f'[{mvp_id}] Completed download raw map {map_name}.png',
+    f'[{mvp_id}] Failed to download raw map {map_name}.png'
+  )
+
+
 def get_mvps_id() -> list[str]:
   print('Fetching mvps ids from divine pride...')
   ids = []
-  for i in range(1, 3):
+  for i in range(1, 2):
     html_src = requests.get(f'https://www.divine-pride.net/database/monster?Flag=4&Page={i}').content
     tree = html.fromstring(html_src)
     mvps_href = tree.xpath('//*[tbody]//*[tr]//*[@class="mvp"]/span/a/@href')
@@ -65,27 +99,16 @@ def get_mvp_info(mvp_id: str, api_key: str) -> Optional[dict]:
   return mvp_info if mvp_info else None
 
 
-def get_mvp_icon(mvp_id: str) -> None:
-  image_path = f'./mvps_icons/{mvp_id}.png'
-  if os.path.exists(image_path):
-    print(f'[{mvp_id}] Icon already exists, skipping...')
-    return
-
-  print(f'[{mvp_id}] Downloading mvp icon {mvp_id}.png')
-  url = f'https://static.divine-pride.net/images/mobs/png/{mvp_id}.png'
-  image_data = requests.get(url).content
-  with open(image_path, 'wb') as image:
-    image.write(image_data)
-    print(f'[{mvp_id}] Completed download mvp icon {mvp_id}.png')
-
-
 def init() -> None:
   try:
-    print(f'MVPs Icons will {"not " if NO_ICONS else ""}be downloaded.')
     print(f'MVPs will {"not " if not USE_FILTER else ""}be filtered.')
+    print(f'MVPs Icons will {"not " if NO_ICONS else ""}be downloaded.')
+    print(f'MVPs Maps will {"not " if NO_MAP_IMGS else ""}be downloaded.')
 
     if not NO_ICONS and not os.path.exists('./mvps_icons/'):
       os.makedirs('./mvps_icons/', exist_ok=True)
+    if not NO_MAP_IMGS and not os.path.exists('./maps/'):
+      os.makedirs('./maps/', exist_ok=True)
     if os.path.exists('./mvps_data.json'):
       override = input('mvps_data.json already exists, override? (y/n) ')
       if override.lower() == 'n':
@@ -110,6 +133,9 @@ def init() -> None:
       mvps_data.append(mvp_info if not USE_FILTER else filter_mvp(mvp_info))
       if not NO_ICONS:
         get_mvp_icon(mvp_id.rstrip('\n'))
+      if not NO_MAP_IMGS:
+        for map_i in mvp_info['spawn']:
+          get_map_img(map_i['mapname'], mvp_id)
     with open('./mvps_data.json', 'w', encoding='utf-8') as mvps_data_file:
       json.dump(mvps_data, mvps_data_file, indent=2)
   except KeyboardInterrupt:
