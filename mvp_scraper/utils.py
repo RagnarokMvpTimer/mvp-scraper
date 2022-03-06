02ecb67ce886ff5fa6c1d88acc2f5d20d9a5cb1c
Lucas8x <lucasemanuel8x@gmail.com>
2022-03-06 00:45:31 -0300

    add utils path functions

--- mvp_scraper/utils.py
+++ mvp_scraper/utils.py
@@ -1,10 +1,31 @@
 import os
+from pathlib import Path
 
 import requests
+from environs import Env
 
 
-def download_img(output_path: str, url: str, on_exist: str, on_start: str, on_complete: str, on_error: str) -> None:
-  if os.path.exists(output_path):
+def get_root_path() -> Path:
+  """
+  Return project root path.
+  :return: PurePath subclass
+  """
+  return Path(__file__).parent.parent
+
+
+def get_output_path() -> Path:
+  env = Env()
+  env.read_env()
+  output_path = env.path('OUTPUT_PATH', './output/')
+
+  if not output_path.is_absolute():
+    output_path = get_root_path().joinpath(output_path)
+
+  return Path(os.path.normcase(output_path)).resolve()
+
+
+def download_img(output_path: Path, url: str, on_exist: str, on_start: str, on_complete: str, on_error: str) -> None:
+  if Path.exists(output_path):
     return print(on_exist)
 
   print(on_start)
@@ -14,6 +35,6 @@ def download_img(output_path: str, url: str, on_exist: str, on_start: str, on_co
     # raise Exception('')
     return print(on_error)
 
-  with open(output_path, 'wb') as file:
-    file.write(data)
+  with output_path.open('wb') as image:
+    image.write(data)
     print(on_complete)
